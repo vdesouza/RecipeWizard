006433eb715a487789d751da1b8bf9ba3ca26fea
Victor De Souza <vdesouza@me.com>
2016-11-14 16:18:44 -0500

    Basic Framework
    Created a MainActivity that houses two fragments: Ingredients and
    Recipes. These fragments are tabbed and can be swiped back and forth.

---
+++ app/src/main/java/com/recipewizard/recipewizard/MainActivity.java
@@ -0,0 +1,104 @@
+package com.recipewizard.recipewizard;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import static android.content.res.Configuration.ORIENTATION_PORTRAIT;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Tabbed main activity and swipe between fragments adapted from these two tutorials:
+    // https://www.youtube.com/watch?v=zQekzaAgIlQ
+    // https://developer.android.com/training/implementing-navigation/lateral.html#tabs
+
+    // ViewPager that will display each section of the app when swiped
+    ViewPager mViewPager;
+
+    // PageAdapter that will provide fragments to each of the two main sections of the app
+    PagerAdapter mPagerAdapter;
+
+    // TabLayout that show the two tabs
+    TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // set up ViewPager, PagerAdapter, and TabLayout
+        mViewPager = (ViewPager) findViewById(R.id.view_pager);
+        mPagerAdapter = new FixedTabsPagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mPagerAdapter);
+
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(mViewPager);
+    }
+
+    // PagerAdapter that displays the two tabs: Ingredients and Recipes
+    public static class FixedTabsPagerAdapter extends FragmentPagerAdapter {
+
+        public FixedTabsPagerAdapter(FragmentManager fm) {
+           super(fm);
+        }
+        @Override
+        public int getCount() {
+            return 2;
+        }
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new IngredientsListFragment();
+                case 1:
+                    return new RecipesListFragment();
+                default:
+                    return null;
+            }
+        }
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "Ingredients";
+                case 1:
+                    return "Recipes";
+                default:
+                    return null;
+            }
+        }
+    }
+
+    // Fragment view for Ingredients list
+    public static class IngredientsListFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_holder, container, false);
+            ((TextView) rootView.findViewById(android.R.id.text1)).setText("Ingredients Place Holder");
+            return rootView;
+        }
+    }
+
+    // Fragment view for Recipes list
+    public static class RecipesListFragment extends Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_holder, container, false);
+            ((TextView) rootView.findViewById(android.R.id.text1)).setText("Recipes Place Holder");
+            return rootView;
+        }
+    }
+}
