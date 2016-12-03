e174119dc1d9360b12f1b8e00f9fb4699bc57914
bhandy02 <bhandy02@users.noreply.github.com>
2016-12-03 18:44:25 -0500

    Add files via upload

--- app/src/main/java/com/recipewizard/recipewizard/FavoritesTabManagerActivity.java
+++ app/src/main/java/com/recipewizard/recipewizard/FavoritesTabManagerActivity.java
@@ -1,5 +1,6 @@
 package com.recipewizard.recipewizard;
 
+import android.os.Environment;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -20,103 +21,87 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
+
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class FavoritesTabManagerActivity extends ListActivity {
+public class FavoritesTabManagerActivity extends AppCompatActivity {
 
     public static final int ADD_FAVORITE_RECIPE_REQUEST = 0;
     public static final int REMOVE_FAVORITE_RECIPE_REQUEST = 1;
     private static final String FILE_NAME = "FavoritesListManagerActivityData.txt";
-    private static final String TAG = "Lab-UserInterface";
+    private static final String TAG = "FavoritesList";
     private static final String NAME = "name";
 
     // IDs for menu items
     private static final int MENU_DELETE = Menu.FIRST;
     private static final int MENU_DUMP = Menu.FIRST + 1;
 
+    ListView listView;
     FavoritesListAdapter mAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "Favorites Manager activity onCreate");
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorites);
+        listView = (ListView) findViewById(R.id.favorites_list);
+        /*
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // However Yanru starts the view Recipe
 
 
+            }
+        });
+        */
         // Create a new TodoListAdapter for this ListActivity's ListView
         mAdapter = new FavoritesListAdapter(this);
 
         // Put divider between ToDoItems and FooterView
-        getListView().setFooterDividersEnabled(true);
+        listView.setFooterDividersEnabled(true);
+        listView.setHeaderDividersEnabled(true);
 
-        // TODO - Inflate footerView for footer_view.xml file
+
         TextView footerView = null;
-        TextView deleteButtonView = null;
+        TextView headerView = null;
         LayoutInflater layoutInflator = LayoutInflater.from(FavoritesTabManagerActivity.this);
-        /*footerView = (TextView) layoutInflator.inflate(R.layout.footer_view, null);
-        deleteButtonView = (TextView) layoutInflator.inflate(R.layout.deletebutton_view,null);
-
-
-        getListView().addFooterView(footerView);
-        getListView().addHeaderView(deleteButtonView);
-
-
-
-
-
-        // TODO - Attach Listener to FooterView
-        footerView.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                final Intent intent = new Intent(BadgesManagerActivity.this, AddBadgeActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
-                startActivityForResult(intent, ADD_BADGE_ITEM_REQUEST);
-            }
-        });
-
-        deleteButtonView.setOnClickListener(new OnClickListener(){
-            @Override
-            public void onClick (View v){
-                Log.d(TAG, "Delte pressed");
-                mAdapter.clear();
-            }
-        });
-        */
-        getListView().setAdapter(mAdapter);
-        // TODO - Attach the adapter to this ListActivity's ListView
-
-
+        listView.setAdapter(mAdapter);
     }
-
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        Log.i(TAG,"Entered onActivityResult()");
-
-        // TODO - Check result code and request code
-        // if user submitted a new Badge
-        // Create a new Badge from the data Intent
-        // and then add it to the adapter
-        if (requestCode == ADD_FAVORITE_RECIPE_REQUEST && resultCode == RESULT_OK) {
-            Recipe recipe = new Recipe(data);
-            Log.d(TAG, "Adding recipe");
-            mAdapter.add(recipe);
-            Log.d(TAG, "Badge recipe");
-        } else if(requestCode == REMOVE_FAVORITE_RECIPE_REQUEST && resultCode == RESULT_OK){
-            String name = data.getStringExtra(NAME);
-            mAdapter.removeRecipeWithName(name);
-
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // only show options menu on portrait view
+        super.onCreateOptionsMenu(menu);
+        Log.d(TAG, "Creating options menu");
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_favorites_actions, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home_button:
+                Intent homeIntent = new Intent(FavoritesTabManagerActivity.this, MainActivity.class);
+                startActivity(homeIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
-
-    // Do not modify below here
 
     @Override
     public void onResume() {
@@ -124,53 +109,38 @@ public class FavoritesTabManagerActivity extends ListActivity {
         super.onResume();
 
         // Load saved ToDoItems, if necessary
-
-        if (mAdapter.getCount() == 0)
+        Log.d(TAG, "Adapter Count = " + mAdapter.getCount());
+        if (mAdapter.getCount() == 0) {
             Log.d(TAG, "Loading items");
-        loadItems();
-
+            loadItems();
+        }
     }
 
     @Override
     protected void onPause() {
         Log.d(TAG, "Pausing");
+
         super.onPause();
 
         saveItems();
 
     }
-    /*
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		super.onCreateOptionsMenu(menu);
-
-		menu.add(Menu.NONE, MENU_DELETE, Menu.NONE, "Delete all");
-		menu.add(Menu.NONE, MENU_DUMP, Menu.NONE, "Dump to log");
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case MENU_DELETE:
-			mAdapter.clear();
-			return true;
-		case MENU_DUMP:
-			dump();
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
-		}
-	}
-    */
     private void saveImage(Bitmap image, String itemName){
         String filename = itemName + ".png";
         FileOutputStream out = null;
-        if(!(new File(filename)).exists()){
+        File root = Environment.getExternalStorageDirectory();
+        File file = new File(root, filename);
+        Log.d(TAG, "Saving: " + file.toString());
+        if(image != null && !file.exists()){
             try {
-                out = new FileOutputStream(filename);
+                out = new FileOutputStream(file);
                 image.compress(Bitmap.CompressFormat.PNG,100, out);
-            } catch(FileNotFoundException e){
+                out.flush();
+                out.close();
+                Log.d(TAG, "Finished Saving: " + out.toString());
+            } catch(FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e){
                 e.printStackTrace();
             } finally{
                 try{
@@ -184,7 +154,9 @@ public class FavoritesTabManagerActivity extends ListActivity {
     }
     private Bitmap loadImage(String itemName){
         String filename = itemName + ".png";
-        return BitmapFactory.decodeFile(filename);
+        String fullPath = Environment.getExternalStorageDirectory()+"/" + filename;
+        Log.d(TAG,"Loading: " + fullPath);
+        return BitmapFactory.decodeFile(fullPath);
     }
     private void dump() {
 
@@ -195,10 +167,27 @@ public class FavoritesTabManagerActivity extends ListActivity {
 
     }
 
+    private boolean[] getAllergyInformationFromLines (String line){
+        String allergiesArray[];
+        boolean allergyInformation [] = new boolean [Recipe.NUM_ALLERGIES];
+        int i = 0;
+        line = line.split("\\[")[1];
+        line = line.split("]")[0];
+        allergiesArray = line.split(",");
+
+        for(String s : allergiesArray){
+            allergyInformation[i] = Boolean.parseBoolean(s);
+            i++;
+        }
+        return allergyInformation;
+    }
+
     private List<Step> getStepsFromLine(String line){
         String stepsArray[], singleStepArray[], singleIngredientArray[] ;
         List<Step> steps = new ArrayList<>();
+        List<String> equipment = new ArrayList<>();
         Bitmap picture = null;
+        String equipmentString [];
         String name;
         String direction;
         boolean checked;
@@ -207,21 +196,29 @@ public class FavoritesTabManagerActivity extends ListActivity {
         String unit;
 
         stepsArray = line.split(";");
-        for(String s : stepsArray){
+        for(String s : stepsArray) {
             singleStepArray = s.split(".");
             List<Ingredient> ingredients = new ArrayList<>();
-            direction = singleStepArray[0];
-            for(int i = 1; i < singleStepArray.length; i++) {
-                singleIngredientArray = singleStepArray[i].split(",");
-                name = singleIngredientArray[0];
-                picture = loadImage(name);
-                checked = Boolean.parseBoolean(singleIngredientArray[1]);
-                category = singleIngredientArray[2];
-                amount = Integer.parseInt(singleIngredientArray[3]);
-                unit = singleIngredientArray[4];
-                ingredients.add(new Ingredient(name, picture, checked, category, amount, unit));
+            if (singleStepArray.length > 0) {
+                direction = singleStepArray[0];
+                for (int i = 1; i < singleStepArray.length - 1; i++) {
+                    singleIngredientArray = singleStepArray[i].split(",");
+                    name = singleIngredientArray[0];
+                    picture = loadImage(name);
+                    checked = Boolean.parseBoolean(singleIngredientArray[1]);
+                    category = singleIngredientArray[2];
+                    amount = Integer.parseInt(singleIngredientArray[3]);
+                    unit = singleIngredientArray[4];
+                    ingredients.add(new Ingredient(name, picture, checked, category, amount, unit));
+                }
+                equipmentString = singleStepArray[singleStepArray.length -1].split(",");
+                if(equipmentString.length > 0);
+                for(String e : equipmentString) {
+                    equipment.add(e);
+                }
+
+                steps.add(new Step(direction, ingredients, equipment));
             }
-            steps.add( new Step(direction,ingredients));
         }
         return steps;
     }
@@ -247,7 +244,7 @@ public class FavoritesTabManagerActivity extends ListActivity {
             int likes;
             int servings;
             int cook_time_in_minutes;
-            boolean [] allergyInformation = new boolean[Recipe.NUM_ALLERGIES];
+            boolean [] allergyInformation;
 
             while (null != (id = reader.readLine())) {
                 name = reader.readLine();
@@ -263,6 +260,7 @@ public class FavoritesTabManagerActivity extends ListActivity {
                 likes = Integer.parseInt(calorieInformationLine[4]);
                 servings = Integer.parseInt(calorieInformationLine[5]);
                 cook_time_in_minutes = Integer.parseInt(calorieInformationLine[6]);
+                allergyInformation = getAllergyInformationFromLines(reader.readLine());
                 mAdapter.add(new Recipe(id, name, author, picture, steps, calories,
                         protein, carbs, fat, likes, servings, cook_time_in_minutes, allergyInformation));
             }
@@ -305,5 +303,111 @@ public class FavoritesTabManagerActivity extends ListActivity {
                 writer.close();
             }
         }
+    }
+    private List<Recipe> getFavoritesListFromFile(){
+        BufferedReader reader = null;
+        List<Recipe> favorites = new ArrayList<>();
+        try {
+            FileInputStream fis = openFileInput(FILE_NAME);
+            reader = new BufferedReader(new InputStreamReader(fis));
+
+            Bitmap picture = null;
+            String id;
+            String name;
+            String stepsString;
+            String author;
+            List<Step> steps = new ArrayList<>();
+            String calorieInformationLine [];
+            int calories;
+            int protein;
+            int carbs;
+            int fat;
+            int likes;
+            int servings;
+            int cook_time_in_minutes;
+            boolean [] allergyInformation;
+
+            while (null != (id = reader.readLine())) {
+                name = reader.readLine();
+                picture = loadImage(name);
+                author = reader.readLine();
+                stepsString = reader.readLine();
+                steps = getStepsFromLine(stepsString);
+                calorieInformationLine = reader.readLine().split(",");
+                calories = Integer.parseInt(calorieInformationLine[0]);
+                protein = Integer.parseInt(calorieInformationLine[1]);
+                carbs = Integer.parseInt(calorieInformationLine[2]);
+                fat = Integer.parseInt(calorieInformationLine[3]);
+                likes = Integer.parseInt(calorieInformationLine[4]);
+                servings = Integer.parseInt(calorieInformationLine[5]);
+                cook_time_in_minutes = Integer.parseInt(calorieInformationLine[6]);
+                allergyInformation = getAllergyInformationFromLines(reader.readLine());
+                favorites.add(new Recipe(id, name, author, picture, steps, calories,
+                        protein, carbs, fat, likes, servings, cook_time_in_minutes, allergyInformation));
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (null != reader) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return favorites;
+    }
+    private void saveNewFavoritesListToFile(List<Recipe> favorites){
+        PrintWriter writer = null;
+        try {
+            FileOutputStream fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(
+                    fos)));
+
+            for (int idx = 0; idx < favorites.size(); idx++) {
+                Recipe curr = (Recipe) favorites.get(idx);
+                saveImage(curr.getPicture(), curr.getName());
+                for(Step step : curr.getSteps())
+                    for(Ingredient ingredient: step.getIngredients())
+                        saveImage(ingredient.getPicture(), ingredient.getName());
+                writer.println(curr);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (null != writer) {
+                writer.close();
+            }
+        }
+    }
+    private void removeRecipeWithName (String name, List<Recipe> favorites){
+        int i = 0;
+        boolean found = false;
+
+        for (Recipe r : favorites){
+            if(r.getName().compareTo(name) == 0) {
+                found = true;
+                break;
+            }
+                i++;
+        }
+        if(found) {
+            favorites.remove(i);
+        }
+
+    }
+    public void addRecipeToFavoritesList(Recipe recipe){
+        List<Recipe> favoritesList = getFavoritesListFromFile();
+        favoritesList.add(recipe);
+        saveNewFavoritesListToFile(favoritesList);
+    }
+    public void removeRecipeFromFavoritesList(Recipe recipe){
+        List<Recipe> favoritesList = getFavoritesListFromFile();
+        removeRecipeWithName(recipe.getName(), favoritesList);
+        saveNewFavoritesListToFile(favoritesList);
     }
 }
