33e1a93a161b6e9f5031e6fabcf172d27b520e19
bhandy02 <bhandy02@users.noreply.github.com>
2016-12-03 17:44:15 -0500

    Add files via upload

--- app/src/main/java/com/recipewizard/recipewizard/Ingredient.java
+++ app/src/main/java/com/recipewizard/recipewizard/Ingredient.java
@@ -9,22 +9,21 @@ import android.os.Parcel;
  * Created by Brian on 11/17/2016.
  */
 
-public class Ingredient implements Parcelable {
+public class Ingredient implements Parcelable{
 
-    private String name = new String();
+    private String name = new String ();
     private Bitmap picture;
     private boolean checked;
     private String category = new String();
-    private double amount;
+    private int amount;
     private String unit;
 
     Ingredient(String name, Bitmap picture, boolean checked, String category){
         this.name = name;
         this.picture = picture;
         this.checked = checked;
-        this.category = category;
     }
-    Ingredient(String name, Bitmap picture, boolean checked, String category, double amount,
+    Ingredient(String name, Bitmap picture, boolean checked, String category, int amount,
                String unit){
         this.name = name;
         this.picture = picture;
@@ -54,17 +53,13 @@ public class Ingredient implements Parcelable {
     }
     public String getCategory() {return category;}
     public void setCategory(String category) {this.category = category;}
-    public double getAmount() {return amount;}
-    public void setAmount(double amount) {this.amount = amount;}
+    public int getAmount() {return amount;}
+    public void setAmount(int amount) {this.amount = amount;}
     public String getUnit () {return unit;}
     public void setUnit (String unit) {this.unit = unit;}
 
     public String toString() {
         return name + "," + checked + "," + category + "," + amount + "," + unit + ",";
-    }
-
-    public String toStringForSaving() {
-        return name + "\n" + checked + "\n" + category + "\n";
     }
 
     @Override
@@ -78,7 +73,7 @@ public class Ingredient implements Parcelable {
         dest.writeValue(picture);
         dest.writeByte((byte) (checked ? 1 : 0));
         dest.writeString(category);
-        dest.writeDouble(amount);
+        dest.writeInt(amount);
         dest.writeString(unit);
     }
 
